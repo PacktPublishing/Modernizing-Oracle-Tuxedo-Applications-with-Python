8881aa016b6bc1f6e7ddc55e739bfae3371085c7
Aivars Kalvans <aivars.kalvans@gmail.com>
2021-02-18 06:26:35 +0000

    format the code

--- 09/dbxa.py
+++ 09/dbxa.py
@@ -3,12 +3,15 @@ import sys
 import tuxedo as t
 import cx_Oracle
 import uuid
+
+
 class Server:
     def tpsvrinit(self, argv):
         t.tpadvertise("STORE")
         t.tpadvertise("COUNTXA")
         self.db = cx_Oracle.connect(handle=t.xaoSvcCtx())
         return 0
+
     def STORE(self, req):
         with self.db.cursor() as dbc:
             dbc.execute(
@@ -16,9 +19,12 @@ class Server:
                 [str(uuid.uuid4()), req["TA_SOURCE"][0]],
             )
         return t.tpreturn(t.TPSUCCESS, 0, req)
+
     def COUNTXA(self, req):
         with self.db.cursor() as dbc:
             dbc.execute("SELECT COUNT(1) FROM messages")
             req["TA_STATUS"] = "Count={}".format(dbc.fetchone()[0])
         return t.tpreturn(t.TPSUCCESS, 0, req)
+
+
 t.run(Server(), sys.argv, "Oracle_XA")
--- 09/dbxathr.py
+++ 09/dbxathr.py
@@ -4,15 +4,21 @@ import tuxedo as t
 import cx_Oracle
 import uuid
 import threading
+
+
 class Server:
     def tpsvrinit(self, argv):
         t.tpadvertise("STORE")
         t.tpadvertise("COUNTXA")
         self.local = threading.local()
         return 0
+
     def tpsvrthrinit(self, argv):
-        self.local.db = cx_Oracle.connect(handle=t.xaoSvcCtx(), threaded=True)
+        self.local.db = cx_Oracle.connect(
+            handle=t.xaoSvcCtx(), threaded=True
+        )
         return 0
+
     def STORE(self, req):
         with self.local.db.cursor() as dbc:
             dbc.execute(
@@ -20,9 +26,12 @@ class Server:
                 [str(uuid.uuid4()), req["TA_SOURCE"][0]],
             )
         return t.tpreturn(t.TPSUCCESS, 0, req)
+
     def COUNTXA(self, req):
         with self.local.db.cursor() as dbc:
             dbc.execute("SELECT COUNT(1) FROM messages")
             req["TA_STATUS"] = "Count={}".format(dbc.fetchone()[0])
         return t.tpreturn(t.TPSUCCESS, 0, req)
+
+
 t.run(Server(), sys.argv, "Oracle_XA")
